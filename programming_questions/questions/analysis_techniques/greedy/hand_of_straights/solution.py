b46070d3ccf3976c310136aaa329f73ad5b1e3ca
edwardxiaoyicong <yxiaoaz@connect.ust.hk>
2024-06-06 17:16:53 +0800

    Update question

---
+++ programming_questions/questions/analysis_techniques/greedy/hand_of_straights/solution.py
@@ -0,0 +1,29 @@
+from utils.solution import Solution
+from collections import Counter
+
+class HandStraights(Solution):
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        # Counter to store the count of each card value
+        card_count = Counter(hand)
+
+        for card in hand:
+            start_card = card
+            # Find the start of the potential straight sequence
+            while card_count[start_card - 1]:
+                start_card -= 1
+
+            # Process the sequence starting from start_card
+            while start_card <= card:
+                while card_count[start_card]:
+                    # Check if we can form a consecutive sequence
+                    # of groupSize cards
+                    for next_card in range(start_card, start_card + groupSize):
+                        if not card_count[next_card]:
+                            return False
+                        card_count[next_card] -= 1
+                start_card += 1
+
+        return True
